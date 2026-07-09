5c8006071420e0d41408630dd98623150cdcd24f
iamzivar <zivarpargati1380@gmail.com>
2026-07-09 23:39:34 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
-def hello():
-    return '<h1>سلام! سایت کار می‌کنه!</h1>'
+def index():
+    return render_template('index.html', lang='fa', tr={'title': 'تست', 'age': 'سن', 'height': 'قد', 'weight': 'وزن', 'submit': 'ادامه'})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
